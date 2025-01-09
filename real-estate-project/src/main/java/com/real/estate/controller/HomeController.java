3b1ca7bbacec9c0407201c441cef4df336fdeb7e
baki-511 <johanbaki381@gmail.com>
2025-01-09 09:31:43 +0530

    completed remaining things

--- real-estate-project/src/main/java/com/real/estate/controller/HomeController.java
+++ real-estate-project/src/main/java/com/real/estate/controller/HomeController.java
@@ -2,14 +2,19 @@ package com.real.estate.controller;
 
 import com.real.estate.service.BlogService;
 import com.real.estate.service.GalleryService;
+import com.real.estate.service.ServicesService;
 import com.real.estate.service.TestimonialService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
+    
+    @Autowired
+    private ServicesService servicesService;
     
     @Autowired
     private TestimonialService testimonialService;
@@ -25,6 +30,7 @@ public class HomeController {
     public String home(Model model) {
         model.addAttribute("testimonials", testimonialService.getAllTestimonial());
         model.addAttribute("logos", galleryService.getAllGallery());
+        model.addAttribute("services", servicesService.getAllServices());
         return "index";
     }
     
@@ -39,20 +45,34 @@ public class HomeController {
     public String about(Model model) {
         model.addAttribute("testimonials", testimonialService.getAllTestimonial());
         model.addAttribute("logos", galleryService.getAllGallery());
+        model.addAttribute("services", servicesService.getAllServices());
         return "/pages/about";
     }
     
     @GetMapping("/blog")
     public String blog(Model model) {
         model.addAttribute("blogs", blogService.getAllBlog());
+        model.addAttribute("services", servicesService.getAllServices());
         return "/pages/blog";
     }
     
-    @GetMapping("/service/1")
-    public String service(Model model) {
+    
+    @GetMapping("/service/{serviceId}")
+    public String getContactUs(@PathVariable Long serviceId, Model model) {
+        model.addAttribute("services", servicesService.getAllServices());
+        model.addAttribute("service", servicesService.getServiceById(serviceId));
         model.addAttribute("testimonials", testimonialService.getAllTestimonial());
         model.addAttribute("logos", galleryService.getAllGallery());
         return "/pages/service";
+    }
+    
+    @GetMapping("/blog/{blogId}")
+    public String getBlogById(@PathVariable Long blogId, Model model) {
+        model.addAttribute("services", servicesService.getAllServices());
+        model.addAttribute("blog", blogService.getBlogById(blogId));
+        model.addAttribute("testimonials", testimonialService.getAllTestimonial());
+        model.addAttribute("logos", galleryService.getAllGallery());
+        return "/pages/blog1";
     }
   
 }
